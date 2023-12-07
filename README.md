bd1b6de1fdcfed91ae4027b706e9438086f98a39
BillClintonAglomasa <aglomasa@yahoo.com>
2023-12-07 14:16:22 +0000

    Adding the contributors, README.md and header file

---
+++ README.md
@@ -0,0 +1,4 @@
+This project (0x16. C - Simple Shell) concentrates on building a simple UNIX command interpreter.
+
+1. AUTHOR
+Contributors to the project.
---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
