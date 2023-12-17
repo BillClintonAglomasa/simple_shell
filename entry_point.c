cf212cd95fc9aa2825f202936992ae6e3adb35db
BillClintonAglomasa <aglomasa@yahoo.com>
2023-12-17 00:13:40 +0000

    Changing the name of print_something.c to entry_point.c

---
+++ entry_point.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * main - The entry point of program
+ *
+ * Return: Always return 0.
+ */
+
+int main(void)
+{
+	prompt(void);
+	return 0;
+}
--- print_something.c
+++
@@ -1,8 +0,0 @@
-#include "shell.h"
-
-int main(void)
-{
-	_print_a_char('g');
-	_print_a_char('\n');
-	return 0;
-}
